0ed74383c05427a77f3fab9f59117ef157f859aa
Wsn-88 <wisnuprayogo@gmail.com>
2024-05-07 22:10:26 +0700

    menambahkan method get & berhasil menampilkan semua data

--- src/controller.js
+++ src/controller.js
@@ -6,3 +6,14 @@ const connection = require('./connection');
 exports.index = function(req, res){
     response.ok("Rest API Berjalan",res);
 }
+
+//menampilkan semua data mahasiswa
+exports.GetAllMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, field ){
+        if (error) {
+            console.error(error);
+        } else {
+            response.ok(rows, res)
+        }
+    }); 
+};
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,5 @@ const controller = require('./controller');
 
 module.exports = function(app){
     app.route('/').get(controller.index);
+    app.route('/mahasiswa').get(controller.GetAllMahasiswa);
 };
